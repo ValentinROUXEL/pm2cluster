b32ae9eb61ca6dc6ff12490b8911a1c272a5579c
ValentinROUXEL <vrouxellycee@gmail.com>
2022-05-30 19:41:57 +0200

    ajout config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,21 @@ module.exports = {
 
   deploy : {
     production : {
+      key: "/publickey.pub",
       user : 'root',
-      host : '127.0.0.1:3232',
+      host : ['127.0.0.1:3232'],
+      ssh_options: "StrictHostKeyChecking=no",
       ref  : 'origin/main',
       repo : 'git@github.com:ValentinROUXEL/pm2cluster.git',
       path : '/home/test',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'pre-setup': 'apt-get install git ; ls -la',
+      'post-setup': "ls -la",
+      // pre-deploy action
+      'pre-deploy-local': "echo 'This is a local executed command'",
+      // post-deploy action
+      'post-deploy': "npm install",
     }
   }
 };
