cb82fdd2314d465d8e97eb3de7f5772f9e4c9b68
ValentinROUXEL <vrouxellycee@gmail.com>
2022-06-03 10:36:08 +0200

    modif configuration deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,17 @@
 module.exports = {
   apps : [{
-    script: './server',
-   }],
-
+    script: 'server.js',
+  }],
+   
+  // Deployment Configuration
   deploy : {
     production : {
-      key: `D:/Documents/pm2petit/pm2cluster/publickey.pub`,
-      user : 'val',
-      host : '192.168.7.14',
-      ssh_options: "StrictHostKeyChecking=no",
-      ref  : 'origin/main',
-      repo : 'git@github.com:ValentinROUXEL/pm2cluster.git',
-      path : '/home/test',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': 'apt-get install git ; ls -la',
-      'post-setup': "ls -la",
-      // pre-deploy action
-      'pre-deploy-local': "echo 'This is a local executed command'",
-      // post-deploy action
-      'post-deploy': "npm install",
+       "user" : "ubuntu",
+       "host" : ["192.168.7.14"],
+       "ref"  : "origin/main",
+       "repo" : "git@github.com:ValentinROUXEL/pm2cluster.git",
+       "path" : "/home/test",
+       "post-deploy" : "npm install; pm2 start server.js"
     }
   }
 };
