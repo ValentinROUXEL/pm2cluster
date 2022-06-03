655afed0810def6971c424c0fe524fc37d2940a0
ValentinROUXEL <vrouxellycee@gmail.com>
2022-06-03 10:45:25 +0200

    rectif config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
        "user" : "val",
        "host" : ["192.168.7.14"],
        "ref"  : "origin/main",
-       "repo" : "git@github.com:ValentinROUXEL/pm2cluster.git",
+       "repo" : "https://github.com/ValentinROUXEL/pm2cluster.git",
        "path" : "/home/test",
        "post-deploy" : "npm install"
     }
